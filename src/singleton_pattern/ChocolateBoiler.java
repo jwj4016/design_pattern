a1e4407c8da75d498fea0a5c094e878e1e4fd144
j <jwj4016@gmail.com>
2023-07-25 17:43:18 +0900

    멀티스레딩 환경에서 싱글톤의 문제점(싱글톤 객체가 여러개 생성되는 문제) 해결을 위한 방법.

--- src/singleton_pattern/ChocolateBoiler.java
+++ src/singleton_pattern/ChocolateBoiler.java
@@ -5,12 +5,30 @@ public class ChocolateBoiler {
     private boolean boiled;
     //ChocolateBoiler 클래스의 하나뿐인 인스턴스 저장하는 정적 변수.
     private static ChocolateBoiler uniqueInstance;
+    //멀티스레딩 환경에서 버그 방지를 위한 방법2.
+    //private static ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+    //멀티스레딩 환경에서 버그 방지를 위한 방법3.(DCL 사용.)
+    //private volatile static ChocolateBoiler uniqueInstance;
+    //public static ChocolateBoiler getInstance() {
+    //  if(uniqueInstance == null) {                        //인스턴스가 있는지 확인하고 없으면 동기화된 블록으로 들어간다.
+    //      synchronized (Singleton.class) {                //처음에만 동기화 된다.
+    //          if(uniqueInstance == null){
+    //              uniqueInstance = new ChocolateBoiler();
+    //          }
+    //      }
+    //      return uniqueInstance;
+    //  }
+    //}
+
 
     private ChocolateBoiler() {
         empty = true;
         boiled = false;
     }
 
+    //멀티스레딩 환경에서 버그 방지를 위한 방법1.
+    //속도가 중요하지 않다면 선택할 수 있는 방법.
+    //1. public static synchronized ChocolateBoiler getInstance() {
     public static ChocolateBoiler getInstance() {
         if (uniqueInstance == null) {
             uniqueInstance = new ChocolateBoiler();
---
+++ src/singleton_pattern/ChocolateClient.java
@@ -0,0 +1,8 @@
+package singleton_pattern;
+
+public class ChocolateClient {
+    public static void main(String[] args) {
+        ChocolateBoilerEnum singleton = ChocolateBoilerEnum.UNIQUE_INSTANCE;
+        singleton.
+    }
+}
